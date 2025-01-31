3cf8e46d667767ade7175eee838f878b2798219b
Renzo Jay Belenzo <ljbelenzo@gmail.com>
2025-01-31 15:52:50 +0800

    allow headers in general

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use('/uploads/images', express.static(path.join('uploads','images')));
 app.use((req,res,next)=>{
     // set headers to API access 
     res.setHeader('Access-Control-Allow-Origin',"https://phonebook-clientside-production.up.railway.app");
-    res.setHeader('Access-Control-Allow-Headers',"Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader('Access-Control-Allow-Headers',"*");
     res.setHeader('Access-Control-Allow-Methods','GET, POST, PATCH, DELETE')
     next();
 });
